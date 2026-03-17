f9f96d78c5f3e825d498fdb7d821d69784362073
NOMBRE <64760106+JeffersonTorres77@users.noreply.github.com>
2026-03-17 07:56:23 -0400

    Update HistorialMedicoController.js

--- src/controllers/HistorialMedicoController.js
+++ src/controllers/HistorialMedicoController.js
@@ -423,9 +423,9 @@ const HistorialMedicoController = {
                 nHistoria: historial.numero,
                 pacienteId: historial.paciente_id,
                 ventaKey: historial.venta_key,
-                pagoPendiente: historial.pago_pendiente,
 
                 datosConsulta: {
+                    pagoPendiente: historial.pago_pendiente,
                     motivo: historial.motivo_consulta,
                     otroMotivo: historial.otro_motivo_consulta,
                     tipoCristalActual: historial.tipo_cristal_actual,
